19e120f2c5470028eb31fcadd565552d8e50700d
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-08-21 17:16:18 -0300

    Adiciona o formulário para ser feito a requisição HTTP para cadastrar posts (faremos o resto semana que vem)

---
+++ src/app/feats/exemplo-http/exemplo-http.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PostRequest } from "./post-request/post-request";
+
+@Component({
+  selector: 'app-exemplo-http',
+  imports: [PostRequest],
+  templateUrl: './exemplo-http.html',
+  styleUrl: './exemplo-http.css',
+})
+export class ExemploHttp {
+
+    
+}
