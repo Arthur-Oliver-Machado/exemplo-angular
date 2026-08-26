ccca28cf8bb1dcc6220903fe24bd935c8d8adefc
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-08-26 14:54:20 -0300

    adiciona exemplo de atualização de post

--- src/app/feats/exemplo-http/consumo-http-service.ts
+++ src/app/feats/exemplo-http/consumo-http-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Post } from './post-request/post';
+import { AtualizaPost } from './put-request/atualiza-post';
 
 @Service()
 export class ConsumoHttpService {
@@ -14,5 +15,7 @@ export class ConsumoHttpService {
         return this.httpClient.post(this.urlApi, postCadastrado);
     }
 
-
+    atualizarPost(postAtualizado: AtualizaPost) {
+        return this.httpClient.put(this.urlApi + '/' + postAtualizado.id, postAtualizado);
+    }
 }
---
+++ src/app/feats/exemplo-http/put-request/put-request.ts
@@ -0,0 +1,48 @@
+import { Component, inject, signal } from '@angular/core';
+import { ConsumoHttpService } from '../consumo-http-service';
+import { AtualizaPost } from './atualiza-post';
+import { form, FormField } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-put-request',
+  imports: [FormField],
+  templateUrl: './put-request.html',
+  styleUrl: './put-request.css',
+})
+export class PutRequest {
+
+  protected readonly consumoService = inject(ConsumoHttpService);
+
+  protected readonly postModel = signal<AtualizaPost>({
+    id: null,
+    userId: null,
+    title: '',
+    body: ''
+  });
+
+  protected readonly postForm = form(this.postModel);
+
+  protected atualizarPost(event: SubmitEvent) {
+    event.preventDefault();
+
+    this.consumoService.atualizarPost(this.postModel()).subscribe({
+      next: () => {
+        alert('Atualização deu certo!!');
+        this.postModel.set({
+          id: null,
+          userId: null,
+          title: '',
+          body: ''
+        });
+        this.postForm().reset();
+      },
+
+      error: () => {
+        alert('Algo deu errado');
+      }
+    })
+
+
+  }
+
+}
